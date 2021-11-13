089aa2356c494ae75991427dc60a74e1c400a807
d-karpov <d-karpov@examplemail.com>
2021-11-13 15:37:31 +0300

    create test collaboration environment

---
+++ MoneyKeeper/MoneyKeeper/ViewControllers/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MoneyKeeper
+//
+//  Created by Денис Карпов on 13.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var firstDev: UILabel!
+    @IBOutlet var secondDev: UILabel!
+    @IBOutlet var thirdDev: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
