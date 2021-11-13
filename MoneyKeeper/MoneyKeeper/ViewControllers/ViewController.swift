972e1f411c45d185f273c744e0e96afae6ecccb1
d-karpov <d-karpov@examplemail.com>
2021-11-13 20:15:38 +0300

    Clean up for pre_main

--- MoneyKeeper/MoneyKeeper/ViewControllers/ViewController.swift
+++ MoneyKeeper/MoneyKeeper/ViewControllers/ViewController.swift
@@ -9,14 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var firstDev: UILabel!
-    @IBOutlet var secondDev: UILabel!
-    @IBOutlet var thirdDev: UILabel!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        thirdDev.text = "Den"
-        secondDev.text = "Ed"
     }
 }
 
