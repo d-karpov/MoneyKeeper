1e24f749b53a488563998f7f34572a04aab53c85
nikita <nikita.dmd@gmail.com>
2021-11-14 23:12:56 +0300

    overview prototype with random values

--- MoneyKeeper/MoneyKeeper/ViewControllers/OverviewViewController.swift
+++ MoneyKeeper/MoneyKeeper/ViewControllers/OverviewViewController.swift
@@ -7,63 +7,69 @@
 
 import UIKit
 
-class OverviewViewController: UITableViewController {
+class OverviewViewController: UIViewController {
     
-//    @IBOutlet var topInfoViewOutlet: UIView!
-//
-//    @IBOutlet var incomeAmountOutlet: UILabel!
-//    @IBOutlet var balanceAmountOutlet: UILabel!
-//    @IBOutlet var withdrawAmountOutlet: UILabel!
-//
+    @IBOutlet var topInfoViewOutlet: UIView!
+
+    @IBOutlet var incomeAmountOutlet: UILabel!
+    @IBOutlet var balanceAmountOutlet: UILabel!
+    @IBOutlet var withdrawAmountOutlet: UILabel!
+    
     private let incomeAmount: Float = 155441.90
     private let withdrawAmount: Float = 900
     private var balanceAmount: Float {
         incomeAmount - withdrawAmount
     }
+    private let amountOfAccounts = Int.random(in: 3...8)
+    private let amountOfWithdraws = Int.random(in: 5...12)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//        incomeAmountOutlet.text = incomeAmount.currencyRU
-//        balanceAmountOutlet.text = withdrawAmount.currencyRU
-//        withdrawAmountOutlet.text = balanceAmount.currencyRU
+
+        incomeAmountOutlet.text = incomeAmount.currencyRU
+        balanceAmountOutlet.text = withdrawAmount.currencyRU
+        withdrawAmountOutlet.text = balanceAmount.currencyRU
     }
     
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-//        topInfoViewOutlet.layer.cornerRadius = view.frame.width / 20
+        topInfoViewOutlet.layer.cornerRadius = view.frame.width / 20
     }
 }
 
-extension OverviewViewController {
-    override func numberOfSections(in tableView: UITableView) -> Int {
+extension OverviewViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
         3
     }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch section {
         case 0: return 1
-        case 1: return Int.random(in: 1...3)
-        default: return Int.random(in: 4...6)
+        case 1: return amountOfAccounts + 1
+        default: return amountOfWithdraws + 1
         }
     }
-    
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         switch section {
         case 0: return "Income"
         case 1: return "Accounts"
         default: return "Withdraw categories"
         }
     }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "overviewRow", for: indexPath)
         var content = cell.defaultContentConfiguration()
-        
-        if indexPath.row == 0 {
-            content.image = UIImage(systemName: "phone")
+
+        if indexPath.section == 0 {
+            content.image = UIImage(systemName: "hand.thumbsup.fill")
+            content.text = incomeAmount.currencyRU
+        } else if indexPath.section == 1 {
+            content.image = UIImage(systemName: "plus.square.dashed")
             content.text = String(indexPath.row)
         } else {
-            content.image = UIImage(systemName: "tray")
+            content.image = UIImage(systemName: "hand.thumbsdown.fill")
             content.text = String(indexPath.row)
         }
 
